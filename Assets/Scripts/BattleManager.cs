0b6791fbb1b67532dec9bdc7cdf8cde0f22165b3
Shuhei YAMADA <duklyon.com@gmail.com>
2020-02-01 14:16:28 +0900

    ボタンを追加した

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class BattleManager : MonoBehaviour
+{
+    [SerializeField] private Button player1AttackRightArmButton;
+    [SerializeField] private Button player1AttackLeftArmButton;
+    [SerializeField] private Button player1RepairRightArmButton;
+    [SerializeField] private Button player1RepairLeftArmButton;
+    [SerializeField] private Button player1RepairBodyButton;
+    
+    [SerializeField] private Button player2AttackRightArmButton;
+    [SerializeField] private Button player2AttackLeftArmButton;
+    [SerializeField] private Button player2RepairRightArmButton;
+    [SerializeField] private Button player2RepairLeftArmButton;
+    [SerializeField] private Button playerRepairBodyButton;
+    
+    
+    
+}
