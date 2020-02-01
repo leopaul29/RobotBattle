eb0042c44aeb1bbf10e8d39c2f1ae975d3a0b83f
Shuhei YAMADA <duklyon.com@gmail.com>
2020-02-01 14:42:52 +0900

    バトルマネージャーを作成中

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -17,7 +18,41 @@ public class BattleManager : MonoBehaviour
     [SerializeField] private Button player2RepairRightArmButton;
     [SerializeField] private Button player2RepairLeftArmButton;
     [SerializeField] private Button playerRepairBodyButton;
+
+    [SerializeField] private Text textBox;
     
+    private int _currentPlayer = 1;
+    private BattleState _currentBattleState = BattleState.BattleStart;
     
-    
+    private enum BattleState
+    {
+        BattleStart,
+        TurnStart,
+        CommandWaiting,
+        CommandResult,
+        BattleEnd,
+    }
+
+    private void Update()
+    {
+        switch (_currentBattleState)
+        {
+            case BattleState.BattleStart:
+                break;
+            case BattleState.TurnStart:
+                break;
+            case BattleState.CommandWaiting:
+                break;
+            case BattleState.CommandResult:
+                break;
+            case BattleState.BattleEnd:
+                break;
+        }
+    }
+
+    private void StartTurn()
+    {
+        textBox.text = string.Format(ConstValue.TurnStartMessage, _currentPlayer);
+        _currentBattleState = BattleState.CommandWaiting;
+    }
 }
---
+++ Assets/Scripts/ConstValue.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConstValue
+{
+    public const string TurnStartMessage = "プレイヤー{0}のターン！";
+}
