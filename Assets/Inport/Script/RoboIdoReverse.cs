cf99f790915455043c95a6d86fff52581c4dcb58
c19021 <c19021@std.it-college.ac.jp>
2020-02-02 00:39:40 +0900

    スプライトの追加

---
+++ Assets/Inport/Script/RoboIdoReverse.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoboIdoReverse : MonoBehaviour
+{
+    bool m_xPlus = true;
+    public float Max;
+    public float Min;
+    bool on = true;
+    public Animator shoot;
+    public GameObject tama;
+    public GameObject enemy;
+    public GameObject DamegeEfect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Player");
+        shoot = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //待機モーションの上下
+        //もし打つアニメーションをしてないとき
+        if (on == true)
+        {
+            if (m_xPlus)
+            {
+                transform.position += new Vector3(0, 0.15f * Time.deltaTime, 0f);
+                if (transform.position.y >= Max)
+                    m_xPlus = false;
+            }
+            else
+            {
+                transform.position -= new Vector3(0, 0.15f * Time.deltaTime, 0f);
+                if (transform.position.y <= Min)
+                    m_xPlus = true;
+            }
+        }
+
+    }
+    //もしボタンをクリックしたとき
+    public void OnClick()
+    {
+        //boolをオンからオフにする
+        on = false;
+        //アニメーションの打ってるときを再生
+        shoot.SetBool("ShootTriger", true);
+        //アニメーションがついた玉オブジェクトを前にだす
+        Instantiate(tama, transform.position + new Vector3(-2, 0, 0), Quaternion.identity);
+        //3秒後にaを読み込む
+        Invoke("a", 3);
+    }
+    void a()
+    {
+        //boolをオンにする
+        on = true;
+        //アニメーションをidolに戻す
+        shoot.SetBool("ShootTriger", false);
+        //3秒後にbを読み込む
+        Invoke("b", 3);
+    }
+    void b()
+    {
+        //敵の位置に弾が当たったかのようなオブジェクトを生み出す
+        Instantiate(DamegeEfect, enemy.transform.position, Quaternion.identity);
+    }
+}
+
---
+++ Assets/Inport/Script/RoboRight.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RoboRight : MonoBehaviour
+{
+    //上昇、下降のbool
+    bool m_xPlus = true;
+    //上昇する距離
+    public float Max;
+    //上昇がマックスに行ったとき下に降りる距離
+    public float Min;
+    public GameObject reza;
+    public GameObject bakuha;
+    public int count;
+    public float Timer;
+    [SerializeField] private ParticleSystem particle;
+    Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        particle.Stop();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count == 1)
+        {
+            if (m_xPlus)
+            {
+                transform.position += new Vector3(1f * Time.deltaTime, 0, 0f);
+                if (transform.position.x >= Max)
+                    m_xPlus = false;
+            }
+            else
+            {
+                transform.position -= new Vector3(1f * Time.deltaTime, 0, 0f);
+                if (transform.position.x <= Min)
+                    m_xPlus = true;
+            }
+            Timer += Time.deltaTime;
+            if (Timer >= 4)
+            {
+                count = 0;
+                particle.Stop();
+                transform.position = pos;
+            }
+        }
+    }
+     public void OnClick()
+    {
+
+
+        Instantiate(reza, transform.position, Quaternion.identity);
+        Invoke("a", 4);
+   
+        
+    }
+    void a()
+    {
+        Instantiate(bakuha, transform.position, Quaternion.identity);
+        Invoke("b", 3.5f);
+    }
+    void b()
+    {        
+        particle.Play();
+        count++;
+    }
+}
