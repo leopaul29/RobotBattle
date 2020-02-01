cfb61be2c1ff29452107a6f8ed44b7bc10da052b
MyName <e175743@ie.u-ryukyu.ac.jp>
2020-02-01 20:07:20 +0900

    make titleScene

---
+++ Assets/Scenes/titlemanage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titlemanage : MonoBehaviour
+{
+
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
