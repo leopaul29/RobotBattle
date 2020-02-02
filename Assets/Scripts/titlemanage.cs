2af34971712170b954f57859a257397d810e8c8e
Shuhei YAMADA <duklyon.com@gmail.com>
2020-02-02 15:22:47 +0900

    タイトル画面でコントローラーのボタンかキーボードを押すとバトルが始まるようにした

--- Assets/Scripts/titlemanage.cs
+++ Assets/Scripts/titlemanage.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement;
 
 public class titlemanage : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Joystick1Button1) || Input.GetKeyDown(KeyCode.L))
+        {
+            OnStartButtonClicked();
+        }
+    }
 
     public void OnStartButtonClicked()
     {
